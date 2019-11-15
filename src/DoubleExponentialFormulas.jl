2aecf3052008a521edf6f2f78a6daafb3638f74a
machakann <mckn@outlook.jp>
2019-11-15 17:17:43 +0800

    Implement quadts()
    Tanh-sinh quadrature which conputes integrals in [-1, 1]

--- src/DoubleExponentialFormulas.jl
+++ src/DoubleExponentialFormulas.jl
@@ -1,5 +1,76 @@
 module DoubleExponentialFormulas
 
-greet() = print("Hello World!")
+using LinearAlgebra: norm
+
+export
+    quadde,
+    quadts,
+    quades,
+    quadss
+
+const h0inv = 8
+const nmax = 1_000_000
+
+function quadde end
+
+function quadts(f::Function, T::Type{<:AbstractFloat}=Float64;
+                rtol::Real=sqrt(eps(float(T))), atol::Real=0,
+                maxeval::Integer=10_000_000)
+    ϕ(t) = tanh(sinh(t)*π/2)
+    ϕ′(t) = (cosh(t)*π/2)/cosh(sinh(t)*π/2)^2
+    deformula(f, ϕ, ϕ′, T, rtol, atol, maxeval)
+end
+
+function quades end
+
+function quadss end
+
+
+# Compute double exponential formula iteratively until satisfying the given tolerance
+function deformula(f::Function, ϕ::Function, ϕ′::Function, T::Type{<:AbstractFloat},
+                   rtol::Real, atol::Real, maxeval::Integer)
+    I0 = f(ϕ(zero(T)))*ϕ′(zero(T))
+    h0 = one(T)/h0inv
+    I, n0 = trapez(f, ϕ, ϕ′, I0, h0, 1, 1, nmax, rtol, atol)
+    h = h0
+    Ih = I*h
+    eval = 0
+    p = 1
+    while eval ≤ maxeval
+        prevIh = Ih
+        h = h0/2^p
+        n = n0*2^p
+        I, k, Ih, tol = trapez(f, ϕ, ϕ′, I, h, 1, 2, n, rtol, atol)
+        eval += k + 1
+        E = norm(prevIh - Ih)
+        !(E > tol) && break
+        p += 1
+    end
+    h*I
+end
+
+
+# Compute the trapezoidal integration with translated integrals and weights
+# Note: ϕ(-t) == -ϕ(t), w(-t) == w(t)
+function trapez(f::Function, ϕ::Function, ϕ′::Function, I, h::Real,
+                kstart::Int, kinc::Int, kend::Int, rtol::Real, atol::Real)
+    tol = zero(float(eltype(I)))
+    Ih = zero(I)
+    lastk = 0
+    for k in kstart:kinc:kend
+        lastk = k
+        x = ϕ(k*h)
+        w = ϕ′(k*h)
+        dI1 = f(x)*w
+        dI2 = f(-x)*w
+        I += dI1
+        I += dI2
+        Ih = I*h
+        tol = max(norm(Ih)*rtol, atol)
+        !(norm(dI1*h) + norm(dI2*h) > tol) && break
+    end
+    I, lastk, Ih, tol
+end
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,57 @@ using DoubleExponentialFormulas
 using Test
 
 @testset "DoubleExponentialFormulas.jl" begin
-    # Write your own tests here.
+    # Test integrals are cited from:
+    # Kahaner, D.K.: Comparison of numerical quadrature formulas, Mathematical
+    # software, Rice,  J.R. (Ed.), Academic Press. pp.229-259 (1971)
+
+    # Test problem 4
+    let
+        f(x::T) where {T<:AbstractFloat} = T(0.92)*cosh(x) - cos(x)
+        f(x::BigFloat) = BigFloat("0.92")*cosh(x) - cos(x)
+        expect = BigFloat("4.7942822668880166735857796183531e-1")
+
+        atol = 1e-6
+        @test isapprox(quadts(f, Float32, rtol=0.0, atol=atol), expect, atol=atol)
+
+        atol = 1e-15
+        @test isapprox(quadts(f, Float64, rtol=0.0, atol=atol), expect, atol=atol)
+
+        atol = 1e-17
+        @test isapprox(quadts(f, BigFloat, rtol=0.0, atol=atol), expect, atol=atol)
+    end
+
+
+    # Test problem 5
+    let
+        f(x::T) where {T<:AbstractFloat} = 1/(x^4 + x^2 + T(0.9))
+        f(x::BigFloat) = 1/(x^4 + x^2 + BigFloat("0.9"))
+        expect = BigFloat("1.5822329637296729331174689490262e0")
+
+        atol = 1e-6
+        @test isapprox(quadts(f, Float32, rtol=0.0, atol=atol), expect, atol=atol)
+
+        atol = 1e-15
+        @test isapprox(quadts(f, Float64, rtol=0.0, atol=atol), expect, atol=atol)
+
+        atol = 1e-17
+        @test isapprox(quadts(f, BigFloat, rtol=0.0, atol=atol), expect, atol=atol)
+    end
+
+
+    # Test problem 20
+    let
+        f(x::T) where {T<:AbstractFloat} = 1/(x^2 + T(1.005))
+        f(x::BigFloat) = 1/(x^2 + BigFloat("1.005"))
+        expect = BigFloat("1.5643964440690497730914930158085e0")
+
+        atol = 1e-6
+        @test isapprox(quadts(f, Float32, rtol=0.0, atol=atol), expect, atol=atol)
+
+        atol = 1e-15
+        @test isapprox(quadts(f, Float64, rtol=0.0, atol=atol), expect, atol=atol)
+
+        atol = 1e-17
+        @test isapprox(quadts(f, BigFloat, rtol=0.0, atol=atol), expect, atol=atol)
+    end
 end
