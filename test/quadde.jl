73d4c9760a78f6b0542cc4b4da5820e3e018c251
machakann <mckn@outlook.jp>
2019-12-14 18:48:01 +0800

    Test the equivalence w/o splitting integral interval

--- test/quadde.jl
+++ test/quadde.jl
@@ -326,7 +326,7 @@ let
 end
 
 
-# Test anti-symmetric equivalence when switching inteval limits with QuadDE
+# Test the anti-symmetric equivalence when switching inteval limits with QuadDE
 # ∫f(x)dx in [a, b] = -∫f(x)dx in [b, a]
 let
     f(x::AbstractFloat) = exp(-x^2)
@@ -635,6 +635,62 @@ let
     I1, E1 = quaddeBF(f,  Inf,  2)
     I2, E2 = quaddeBF(f, -2, -Inf)
     @test I1 ≈ -I2
+    @test E1 ≤ sqrt(eps(I1))*norm(I1)
+    @test E2 ≤ sqrt(eps(I2))*norm(I2)
+end
+
+
+# Test the equivalence w/o splitting integral interval
+# ∫f(x)dx in [a, b] = ∫f(x)dx in [a, c] + ∫f(x)dx in [c, b]  where a ≤ c ≤ b
+let
+    f(x::AbstractFloat) = exp(-x^2)
+
+    I,  E  = quadde32(f, -1, 1)
+    I1, E1 = quadde32(f, -1, 0)
+    I2, E2 = quadde32(f, 0, 1)
+    @test I ≈ I1 + I2
+    @test E  ≤ sqrt(eps(I))*norm(I)
+    @test E1 ≤ sqrt(eps(I1))*norm(I1)
+    @test E2 ≤ sqrt(eps(I2))*norm(I2)
+
+    I,  E  = quadde64(f, -1, 1)
+    I1, E1 = quadde64(f, -1, 0)
+    I2, E2 = quadde64(f, 0, 1)
+    @test I ≈ I1 + I2
+    @test E  ≤ sqrt(eps(I))*norm(I)
+    @test E1 ≤ sqrt(eps(I1))*norm(I1)
+    @test E2 ≤ sqrt(eps(I2))*norm(I2)
+
+    I,  E  = quaddeBF(f, -1, 1)
+    I1, E1 = quaddeBF(f, -1, 0)
+    I2, E2 = quaddeBF(f, 0, 1)
+    @test I ≈ I1 + I2
+    @test E  ≤ sqrt(eps(I))*norm(I)
+    @test E1 ≤ sqrt(eps(I1))*norm(I1)
+    @test E2 ≤ sqrt(eps(I2))*norm(I2)
+
+
+    I,  E  = quadde32(f, -Inf, Inf)
+    I1, E1 = quadde32(f, -Inf, 0)
+    I2, E2 = quadde32(f, 0, Inf)
+    @test I ≈ I1 + I2
+    @test E  ≤ sqrt(eps(I))*norm(I)
+    @test E1 ≤ sqrt(eps(I1))*norm(I1)
+    @test E2 ≤ sqrt(eps(I2))*norm(I2)
+
+    I,  E  = quadde64(f, -Inf, Inf)
+    I1, E1 = quadde64(f, -Inf, 0)
+    I2, E2 = quadde64(f, 0, Inf)
+    @test I ≈ I1 + I2
+    @test E  ≤ sqrt(eps(I))*norm(I)
+    @test E1 ≤ sqrt(eps(I1))*norm(I1)
+    @test E2 ≤ sqrt(eps(I2))*norm(I2)
+
+    I,  E  = quaddeBF(f, -Inf, Inf)
+    I1, E1 = quaddeBF(f, -Inf, 0)
+    I2, E2 = quaddeBF(f, 0, Inf)
+    @test I ≈ I1 + I2
+    @test E  ≤ sqrt(eps(I))*norm(I)
     @test E1 ≤ sqrt(eps(I1))*norm(I1)
     @test E2 ≤ sqrt(eps(I2))*norm(I2)
 end
